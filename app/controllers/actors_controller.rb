0467f188c7c1c708edcbf22ca1253c3f3ac2f404
susird <susir95@gmail.com>
2018-12-03 18:11:19 -0500

    Listando actores

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,12 +1,6 @@
 class ActorsController < ApplicationController
     def index 
-        @pic = "Foto: "
-        @name = "Nombre: "
-        @bio = "Bio: "
-        @born = "Fecha de nacimiento: "
-        @lugborn = "Lugar de nacimiento: "
-        @die = "Fecha de fallecimiento: "
-        @place = "Lugar de fallecimiento: "
-    end
+        @actors = Actor.all
+     end
 end
 
